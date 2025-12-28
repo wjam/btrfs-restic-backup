d9ca90333c178405f0cb79cf44bcdcab588b697a
Will May <will.j.may@gmail.com>
2025-12-28 15:05:32 +0000

    .

---
+++ .github/run_and_wait_for_unit_to_finish.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then
+  set -o xtrace
+fi
+
+unit=${1:?Unit name required}
+
+systemctl --user daemon-reload
+systemctl --user start "$unit"
+
+sleep 5
+
+while systemctl is-active --user --quiet "$unit"; do
+  sleep 5
+  echo "."
+  journalctl --lines=20 -u "$unit"
+done
---
+++ scripts/btrfs-restic-backup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+set -o xtrace
+# TODO disable xtrace
+
+snapshot_name="@restic-snapshot"
+
+# TODO can we get the BTRFS_VOL from the BTRFS_SUBVOL?
+snapshot="$BTRFS_VOL/$snapshot_name"
+subvolume="$BTRFS_SUBVOL"
+# TODO lookup device?
+btrfs_dev="$BTRFS_DEVICE"
+
+# clean old snapshot
+if btrfs subvolume delete "$snapshot"; then
+  echo "WARNING: previous run did not cleanly finish, removed old snapshot"
+fi
+
+btrfs subvolume snapshot -r "$subvolume" "$snapshot"
+trap 'btrfs subvolume delete $snapshot' EXIT
+
+umount "$subvolume"
+
+# TODO
+ls -l "$subvolume"
+
+mount --types btrfs --options "subvol=$snapshot_name" "$btrfs_dev" "$subvolume"
+
+# TODO Default BACKUP_PATHS to $BTRFS_SUBVOL?
+#resitc ${RESTIC_CACHE:-} backup --verbose --one-file-system ${BACKUP_EXCLUDES:-} ${BACKUP_PATHS}
+
+# TODO
+ls -l "$subvolume"
