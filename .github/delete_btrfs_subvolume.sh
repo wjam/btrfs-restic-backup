27a6a592f482b76e1d2dc7310a6a3432bbba5d87
Will May <will.j.may@gmail.com>
2025-12-30 10:02:18 +0000

    .

---
+++ .github/delete_btrfs_subvolume.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then
+  set -o xtrace
+fi
+
+root_vol=${2:?root volume path required}
+sub_vol=${3:?subvolume name required}
+sub_vol_mount=${4:?subvolume mount path required}
+
+sudo umount "$sub_vol_mount"
+sudo btrfs subvolume delete "$root_vol/$sub_vol"
