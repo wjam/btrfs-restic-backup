1361441ea98b1ecb474313b0a25d316a1dfe0610
Will May <will.j.may@gmail.com>
2025-12-30 17:18:18 +0000

    .

---
+++ scripts/btrfs-restic-backup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+set -o xtrace
+# TODO disable xtrace
+
+subvolume="$BTRFS_SUBVOL"
+btrfs_dev="$(findmnt --types btrfs --options subvol --target "$subvolume" --nofsroot --output source --noheadings)"
+
+echo "Replacing subvolume with snapshot"
+umount --verbose "$subvolume" 2>&1
+
+mount --types btrfs --options "subvol=$SNAPSHOT_NAME" "$btrfs_dev" "$subvolume"
+
+restic "$@"
