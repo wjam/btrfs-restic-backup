edc0188f62deba54bf3bfd399b5d72ad33952a10
Will May <will.j.may@gmail.com>
2025-12-30 10:28:50 +0000

    prune

--- scripts/btrfs-restic-backup.sh
+++ scripts/btrfs-restic-backup.sh
@@ -15,7 +15,8 @@ set -o xtrace
 # TODO split this script up?
 #  - ExecStartPre which creates the snapshot
 #  - ExecStart which performs the backup
-#  - ExecSStopPost which deletes the completed snapshot
+#  - ExecStartPost which performs the trimming
+#  - ExecStopPost which deletes the completed snapshot
 # Splitting up would allow _another_ ExecStartPre to be added which injects another file into the subvolume & lists the contents to prove the files backed up are the snapshot
 # https://unix.stackexchange.com/questions/348450/confused-by-execstartpre-entries-in-systemd-unit-file
 # As the service is a `oneshot`, multiple ExecStart could be added to the unit via `.d` directory backing up to many locations
@@ -57,4 +58,8 @@ sudo mount --types btrfs --options "subvol=$snapshot_name" "$btrfs_dev" "$subvol
 # TODO Default BACKUP_PATHS to $BTRFS_SUBVOL?
 restic ${RESTIC_CACHE:-} backup --verbose --one-file-system ${BACKUP_EXCLUDES:-} ${BACKUP_PATHS}
 
-# TODO trim off any unwanted backups
+restic ${RESTIC_CACHE:-} forget --prune --max-unused 5G --verbose \
+    --keep-daily "${RETENTION_DAYS:-7}" \
+    --keep-weekly "${RETENTION_WEEKS:-4}" \
+    --keep-monthly "${RETENTION_MONTHS:-6}" \
+    --keep-yearly "${RETENTION_YEARS:-0}"
