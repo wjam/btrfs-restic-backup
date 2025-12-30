bd770ff2b980c9d999cc0a8186ca89c86412d1e0
Will May <will.j.may@gmail.com>
2025-12-30 12:31:29 +0000

    Better snapshot name

--- scripts/btrfs-restic-backup.sh
+++ scripts/btrfs-restic-backup.sh
@@ -21,15 +21,12 @@ set -o xtrace
 # https://unix.stackexchange.com/questions/348450/confused-by-execstartpre-entries-in-systemd-unit-file
 # As the service is a `oneshot`, multiple ExecStart could be added to the unit via `.d` directory backing up to many locations
 
-# TODO better name for this snapshot? From systemd unit name? '%N'
-snapshot_name="@btrfs-restic-backup"
-
 # TODO Given the subvolume path, can the btrfs volume & subvolume name be found?
 # If so, when using PrivateTmp, the root can be mounted to /tmp/btrfs and actions performed there
 # thus avoiding the need for the root volume to be always mounted - something the flat layout says isn't needed
 
 # TODO can we get the BTRFS_VOL from the BTRFS_SUBVOL?
-snapshot="$BTRFS_VOL/$snapshot_name"
+snapshot="$BTRFS_VOL/$SNAPSHOT_NAME"
 subvolume="$BTRFS_SUBVOL"
 # TODO lookup device?
 btrfs_dev="$BTRFS_DEVICE"
@@ -47,7 +44,7 @@ trap 'btrfs subvolume delete $snapshot' EXIT
 echo "Replacing subvolume with snapshot"
 umount --verbose "$subvolume" 2>&1
 
-mount --types btrfs --options "subvol=$snapshot_name" "$btrfs_dev" "$subvolume"
+mount --types btrfs --options "subvol=$SNAPSHOT_NAME" "$btrfs_dev" "$subvolume"
 
 # TODO Default BACKUP_PATHS to $BTRFS_SUBVOL?
 restic ${RESTIC_CACHE:-} backup --verbose --one-file-system ${BACKUP_EXCLUDES:-} ${BACKUP_PATHS}
