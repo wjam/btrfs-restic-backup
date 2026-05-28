96c771ac49e47b63e9b8c9360cfe9ccc9ee63dca
Will May <will.j.may@gmail.com>
2026-05-28 20:20:26 +0100

    Add test to verify backup can be restored
    Verify a backup performed on Ubuntu 24.04 can be restored.

---
+++ .github/verify_restore.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then
+  set -o xtrace
+fi
+
+root="$(git rev-parse --show-toplevel)"
+
+out="$(mktemp -d)"
+
+restic restore --target "$out" latest
+
+tree "$out/repo/content"
+
+diff "$root/testdata/content" "$out/repo/content"
